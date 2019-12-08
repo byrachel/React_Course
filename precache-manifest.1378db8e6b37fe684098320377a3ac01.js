884c6e7df1602d11187ed4323fa726af5e70de74
Rachel Nething <byrachel@gmail.com>
2019-12-08 10:48:21 +0100

    Updates

---
+++ precache-manifest.1378db8e6b37fe684098320377a3ac01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9138f393c8495bd756b1fd14c80175e",
+    "url": "/appcontacts/index.html"
+  },
+  {
+    "revision": "d5ae0c80941080902998",
+    "url": "/appcontacts/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a5fdc7ed01f7f3941a59",
+    "url": "/appcontacts/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "d5ae0c80941080902998",
+    "url": "/appcontacts/static/js/2.ffbd6d0b.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/appcontacts/static/js/2.ffbd6d0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5fdc7ed01f7f3941a59",
+    "url": "/appcontacts/static/js/main.7be91f72.chunk.js"
+  },
+  {
+    "revision": "909f290f127dfe8f5960",
+    "url": "/appcontacts/static/js/runtime-main.d9d46134.js"
+  }
+]);
