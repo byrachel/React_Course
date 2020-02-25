f9302cf8b31c5e31a3731ad1954641945992623c
Rachel Nething <byrachel@gmail.com>
2020-02-25 17:20:20 +0100

    useEffet

--- src/composants/hook.js
+++ src/composants/hook.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
+import axios from 'axios';
 
 function Hook() {
 
+// useState
+
   const [ initialState, changeState ] = useState(1);
+  const [ tableauVide, remplirTab ] = useState([0]);
 
   const increment = () => {
     changeState(initialState+1)
   }
 
+  const tableau = () => {
+    for(var i = 0; i > 0; i++) {
+      remplirTab(tableauVide.push(initialState));
+    }
+    console.log(tableauVide);
+  }
+
+// userEffect (similaire à componentDidMount et componentDidUpdate)
+
+  const [ data, setData ] = useState([]);
+
+  useEffect(() => {
+      const fetchData = async() => {
+          const result = await axios(
+              'https://jsonplaceholder.typicode.com/posts',
+          );
+          setData(result.data);
+      }
+      fetchData();
+    }, []
+  )
+
+
   return (
     <div className="container">
 
-      <p>Le state initial est de : {initialState} </p>
-      <button onClick={increment}>J'aime !</button>
-      
+    <h2>UseState</h2>
+        <p>Le state initial est de : {initialState} </p>
+        <button onClick={increment}>J'aime !</button>
+        <button onClick={tableau}>Enregistrer dans la console</button>
+    <p>_</p>
+    <h2>UseEffect</h2>
+        <Fragment>
+            <ul>
+                {data.map(item => (
+                    <li key={item.id}><p>{item.title}</p></li>
+                ))}
+            </ul>
+        </Fragment>
     </div>
   );
 }
